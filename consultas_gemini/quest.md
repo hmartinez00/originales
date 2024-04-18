b329e87791930c97c052e90fbce731f9cb02abda
hmartinez <hmartinez@abae.gob.ve>
2024-04-18 00:45:26 -0400

    Version Estable 20240418 004525

--- consultas_gemini/quest.md
+++ consultas_gemini/quest.md
@@ -1,17 +1,3 @@
-Quita todos los acentos de los valores del diccionario. No toques las claves del diccionario:
+Voy a publicar un video de esta receta en Facebook e instagram. Crea un copy atractivo con estructura AIDA, de 5 frases de menos de 100 caracteres para crear el texto de la publicacion. Agrega al final la lista de ingredientes de esta receta y sus cantidades y 9 Hashtags en castellano. No digas nada sobre su preparacion.
 
-data = {
-    'https://vm.tiktok.com/ZMMXaYT2C/': 'recetas de trufas de chocolate',
-    'https://vm.tiktok.com/ZMMXa4nVG/': 'técnica de temperado del chocolate',
-    'https://vm.tiktok.com/ZMMXa3Kto/': 'ciencia del chocolate brillante',
-    'https://vm.tiktok.com/ZMMXav1pJ/': 'bombón de chocolate casero',
-    'https://vm.tiktok.com/ZMMXmL5rW/': 'arte de la escultura de chocolate',
-    'https://vm.tiktok.com/ZMMXmj54G/': 'beneficios del cacao para la salud',
-    'https://vm.tiktok.com/ZMMXmVQ8j/': 'recetas saludables con chocolate',
-    'https://vm.tiktok.com/ZMMXmCRmG/': 'alternativas al azúcar en las',
-    'https://vm.tiktok.com/ZMMXmuSnD/': 'recetas de chocolate',
-    'https://vm.tiktok.com/ZMMXmCe26/': 'chocolate personalizado para eventos',
-    'https://vm.tiktok.com/ZMMXms1w5/': 'regalos de chocolate para San Valentín',
-    'https://vm.tiktok.com/ZMMXuMuK6/': 'chocolate temático para fiestas',
-    'https://vm.tiktok.com/ZMMXmw4ha/': 'cursos de fabricación de chocolate en línea',
-}
+Error
--- scripts/translate.py
+++ scripts/translate.py
@@ -7,7 +7,7 @@ import json
 import os
 import tkinter as tk
 from tkinter import filedialog
-from sync_voice_over.sync_voice_over import find_files
+# from General_Utilities.sync_voice_over import find_All_files
 
 
 sys.stdout.reconfigure(encoding='utf-8')
@@ -16,31 +16,68 @@ sys.stdout.reconfigure(encoding='utf-8')
 modelo = genai.GenerativeModel('gemini-pro')
 
 # Abrir el archivo JSON
-with open('settings.json', 'r') as f:
+with open(r'settings\api\settings.json', 'r') as f:
     data = json.load(f)
 
 # Configuramos la API KEY 
 GOOGLE_API_KEY=data['api']
 genai.configure(api_key=GOOGLE_API_KEY)
- 
-# Rebajamos el tamaño de la respuesta de la IA 
-def rebajar(text):
-    text = text.replace('•', '  *')
-    return Markdown(textwrap.indent(text, '> ', predicate=lambda _: True))
 
-def ai_query(model, command, output_file):
-    # Obtenemos la respuesta 
-    ask = model.generate_content(command)
-    ask = ask.text
-    # ask = ask.candidates[0].content.parts
-    # print(ask)
-    # with open(output_file, 'w', encoding='utf-8') as f:
-    #     f.writelines(ask)
+def process_command(model, _directory, ext, final_ext, command):
+    import os
+    import subprocess
 
-    return ask
+    # Rebajamos el tamaño de la respuesta de la IA 
+    def rebajar(text):
+        text = text.replace('•', '  *')
+        return Markdown(textwrap.indent(text, '> ', predicate=lambda _: True))
 
-def process_command(ext, final_ext, command):
-    files = find_files(directory, ext)
+    def ai_query(model, command):
+        # Obtenemos la respuesta 
+        ask = model.generate_content(command)
+        ask = ask.text
+        # ask = ask.candidates[0].content.parts
+        # print(ask)
+        # with open(output_file, 'w', encoding='utf-8') as f:
+        #     f.writelines(ask)
+
+        return ask
+
+    def ai_secure_query(command):
+        # Obtenemos la respuesta 
+        input_file = r'consultas_gemini\quest.md'
+        output_file = r'consultas_gemini\ask.md'
+
+        with open(input_file, 'w', encoding='utf-8') as f:
+            f.writelines(command)
+        subprocess.run(["python", "scripts/app.py"])
+
+        while True:
+            eleccion = input("¿Desea conservar las respuesta (S/N)? ")
+            if eleccion.lower() == "n":
+                subprocess.run(["python", "scripts/app.py"])
+
+            elif eleccion.lower() == "s":
+                break
+            else:
+                print("Opción no válida. Ingrese 'S' para continuar o 'N' para detenerse.")
+
+        with open(output_file, 'r', encoding='utf-8') as archivo:
+            # Leer el contenido del archivo
+            r_linea = archivo.read()
+        ask = r_linea
+
+        return ask
+
+    def find_All_files(_directory, ext):
+        f_files = []
+        for root, _, files in os.walk(_directory):
+            for file in files:
+                if file.endswith(ext):
+                    f_files.append(os.path.join(root, file))
+        return f_files
+
+    files = find_All_files(_directory, ext)
     for file in files:
         print(file)
         # Abrir el archivo en modo lectura
@@ -53,16 +90,19 @@ def process_command(ext, final_ext, command):
         output_file = os.path.join(dirname, filename)
         
         prompt = \
-    f'''{command}
+f'''{command}
 
-    {r_linea}
-    '''
-        ask = ai_query(modelo, prompt, output_file)
-        print(ask)
+{r_linea}'''
 
+        print(output_file)
+        # try:
+        #     ask = ai_query(model, prompt)
+        # except OSError as e:
+        #     ask = f"Error creating file: {e}"
+        ask = ai_secure_query(prompt)
         with open(output_file, 'w', encoding='utf-8') as f:
             f.writelines(ask)
-
+        
 
 
 # Seleccionamos el directorio
@@ -73,11 +113,11 @@ directory = filedialog.askdirectory()
 
 ext1 = '.srt'
 final_ext1 = ' - traducido.srt'
-command1 = 'Traduce el siguiente archivo de subtitulos al español. Entrega el resultado en formato de archivo srt con las misma marcas de tiempo que ya tiene.'
-process_command(ext1, final_ext1, command1)
+command1 = 'Traduce el siguiente archivo de subtitulos al castellano. Entrega el resultado en formato de archivo srt con las misma marcas de tiempo que ya tiene.'
+process_command(modelo, directory, ext1, final_ext1, command1)
 
 ext2 = 'traducido.srt'
 final_ext2 = ' - copy.md'
-command2 = 'Voy a publicar un video de esta receta en Facebook e instagram. Crea un copy atractivo con estructura AIDA, de 5 frases de menos de 100 caracteres para crear el texto de la publicación. Agrega al final la lista de ingredientes de esta receta y sus cantidades y 9 Hashtags en español. No digas nada sobre su preparación.'
-process_command(ext2, final_ext2, command2)
+command2 = 'Voy a publicar un video de esta receta en Facebook e instagram. Crea un copy atractivo con estructura AIDA, de 5 frases de menos de 100 caracteres para crear el texto de la publicacion. Agrega al final la lista de ingredientes de esta receta y sus cantidades y 9 Hashtags en castellano. No digas nada sobre su preparacion.'
+process_command(modelo, directory, ext2, final_ext2, command2)
 
